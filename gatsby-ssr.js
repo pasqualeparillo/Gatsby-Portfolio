7f0813944a9afbce79335497dfc7499c08d1a95e
pasqualeparillo <patparillo@gmail.com>
2019-08-17 12:38:39 -0400

    test ssr context fix replaceRenderer bodyComponent

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,3 +5,12 @@
  */
 
 // You can delete this file if you're not using it
+import React from "react"
+import { renderToString } from "react-dom/server"
+import { ContextProvider } from "./src/components/store/state"
+
+export const replaceRenderer = ({ bodyComponent, replaceBodyHTMLString }) => {
+  // React Context in SSR/build
+  const ConnectedBody = () => <ContextProvider>{bodyComponent}</ContextProvider>
+  replaceBodyHTMLString(renderToString(<ConnectedBody />))
+}
