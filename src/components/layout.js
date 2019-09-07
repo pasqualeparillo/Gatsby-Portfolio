338efecbb0ea7a16417f81e02bd50dd6cfb9e443
pasqualeparillo <patparillo@gmail.com>
2019-09-07 12:12:42 -0400

    NavBar test

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,12 +1,10 @@
 import React from "react"
 import PropTypes from "prop-types"
 import NavBar from "./navigation/navBar"
-import Logo from "./navigation/logo"
 import { ContextProvider } from "./store/state"
 import { AnimatePresence, motion } from "framer-motion"
 import "./layout.css"
 import "./style.css"
-import SocialLinks from "./navigation/socialLinks"
 import CopyRight from "./navigation/copyRight"
 
 export const Layout = ({ children, location }) => {
@@ -21,6 +19,7 @@ export const Layout = ({ children, location }) => {
   return (
     <ContextProvider>
       <div className="flex flex-col min-h-screen w-full bg-white">
+        <NavBar />
         <AnimatePresence>
           <motion.div
             className="flex flex-1 flex-col flex-grow justify-center min-h-screen"
